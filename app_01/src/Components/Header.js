b4d3d5fff5a1a6136b42965f4ad2375e8f1058c5
sher <sher.kg15@gmail.com>
2023-01-03 07:24:07 +0600

    Роутинг сайта, переход по страницам

--- app_01/src/Components/Header.js
+++ app_01/src/Components/Header.js
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Contacts from "../Pages/Contacts";
+import Blog from "../Pages/Blog";
+
 import {
   Button,
   Container,
@@ -12,43 +18,59 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <Navbar
-          fixed="top"
-          collapseOnSelect
-          expand="md"
-          bg="dark"
-          variant="dark"
-        >
-          <Container>
-            <Navbar.Brand href="/">
-              <img
-                src={logo}
-                height="30"
-                width="30"
-                className="d-inline-block aling-top"
-                alt="Logo"
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/About">About</Nav.Link>
-                <Nav.Link href="/Contacts">Contacts</Nav.Link>
-                <Nav.Link href="/Blog">Blog</Nav.Link>
-              </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="text"
-                  placeholder="Search"
-                  className="d-inline mx-2"
+        <Router>
+          <Navbar
+            sticky="top"
+            collapseOnSelect
+            expand="md"
+            bg="dark"
+            variant="dark"
+          >
+            <Container>
+              <Navbar.Brand href="/">
+                <img
+                  src={logo}
+                  height="30"
+                  width="30"
+                  className="d-inline-block aling-top"
+                  alt="Logo"
                 />
-                <Button variant="outline-info">Search</Button>
-              </Form>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        <div className="logo">PORTEN</div>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link as={Link} to="/">
+                    Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/about">
+                    About
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/contacts">
+                    Contacts
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/blog">
+                    Blog
+                  </Nav.Link>
+                </Nav>
+                <Form className="d-flex">
+                  <FormControl
+                    type="text"
+                    placeholder="Search"
+                    className="d-inline mx-2"
+                  />
+                  <Button variant="outline-info">Search</Button>
+                </Form>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contacts" element={<Contacts />} />
+            <Route exact path="/blog" element={<Blog />} />
+          </Routes>
+        </Router>
       </div>
     );
   }
