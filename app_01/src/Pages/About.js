87c255e76e2e8a5a148ca8ead0e2e157a9b7c2b2
main <you@example.com>
2023-01-12 22:07:24 +0600

    implemented about page

--- app_01/src/Pages/About.js
+++ app_01/src/Pages/About.js
@@ -1,7 +1,84 @@
 import React, { Component } from "react";
-
+import { Container, Nav, Row, Col, Tab } from "react-bootstrap";
+import firstImg from "../assets/first.jpeg";
+import secondImg from "../assets/second.jpeg";
 export default class About extends Component {
   render() {
-    return <div>About</div>;
+    return (
+      <Container>
+        <Tab.Container id="ledt-tabs-exaple" defaultActiveKey="first">
+          <Row>
+            <Col sm={2}>
+              <Nav variant="pills" className="flex-column mt-2">
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Desing</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Team</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="third">Programming</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="fourth">Framworks</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="fifth">libraries</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col sm={9}>
+              <Tab.Content className="mt-3">
+                <Tab.Pane eventKey="first">
+                  <img src={firstImg} />
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Enim dolore laudantium adipisci odio placeat eum sit sint
+                    consequuntur dolorum soluta, ipsa quos provident et eius.
+                    Voluptatem ea fuga quo maiores?
+                  </p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="second">
+                  <img src={secondImg} />
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Enim dolore laudantium adipisci odio placeat eum sit sint
+                    consequuntur dolorum soluta, ipsa quos provident et eius.
+                    Voluptatem ea fuga quo maiores?
+                  </p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="third">
+                  <img src={firstImg} />
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Enim dolore laudantium adipisci odio placeat eum sit sint
+                    consequuntur dolorum soluta, ipsa quos provident et eius.
+                    Voluptatem ea fuga quo maiores?
+                  </p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="fourth">
+                  <img src={secondImg} />
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Enim dolore laudantium adipisci odio placeat eum sit sint
+                    consequuntur dolorum soluta, ipsa quos provident et eius.
+                    Voluptatem ea fuga quo maiores?
+                  </p>
+                </Tab.Pane>
+                <Tab.Pane eventKey="fifth">
+                  <img src={firstImg} />
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Enim dolore laudantium adipisci odio placeat eum sit sint
+                    consequuntur dolorum soluta, ipsa quos provident et eius.
+                    Voluptatem ea fuga quo maiores?
+                  </p>
+                </Tab.Pane>
+              </Tab.Content>
+            </Col>
+          </Row>
+        </Tab.Container>
+      </Container>
+    );
   }
 }
